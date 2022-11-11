ecf8cfe0b7ce8778cd4387dd892ca1eccc24e8a1
Maddy-176 <kummmx176@gmail.com>
2022-11-11 12:14:32 +0530

    removing dependecies

--- src/components/ScrollComponent.jsx
+++ src/components/ScrollComponent.jsx
@@ -1,71 +1,69 @@
 import React,{useState,useEffect, useRef,useCallback} from 'react'
 import axios from "axios";
+import "../style/scroll.css"
+
 
 const ScrollComponent=()=>{
     const[photos,setPhotos]= useState([]);
     const[loading, setLoading] = useState(false);
     const [page, setPage]= useState(0);
-    const [prevY, setPrevY]= useState(0);
 
     const observerRef= useRef();
 
     const getPhotos=useCallback(async (page)=>{
         setLoading(true)
-        const response =await axios.get(`https://jsonplaceholder.typicode.com/photos?_page=${page}&_limit=10`);
+        const response =await axios.get(`https://api.thecatapi.com/v1/images/search?limit=9&page=${page}&order=Desc`);
          await setPhotos([...photos,...response.data]);
          await setLoading(false)
     },[photos])
     
 
     const loadingCSS = {
-        height: "100px",
+        height: "30px",
         margin: "30px"
       };
 
-    const handleObserver=useCallback((entities,observer)=>{
-        const y=entities[0].boundingClientRect.y;
-        if(prevY>y){
-            const lastPhoto= photos[photos.length-1];
-            const currPage= lastPhoto.albumId;
-            console.log("currpage",currPage)
-            getPhotos(currPage);
-            setPage(currPage);
+  
+
+    
+    const handleObserver=useCallback((entries)=>{
+        const target= entries[0]
+        if(target.isIntersecting){
+            setPage(prev=>prev+1)
         }
-        setPrevY(y);
       
 
-    },[getPhotos,photos,prevY]
-    )
+    },[])
 
     useEffect(()=>{
         getPhotos(page);
 
         var options = {
             root: null,
-            rootMargin: "1px",
+            rootMargin: "50px",
             threshold: 1.0
           };
 
           const observer = new IntersectionObserver(handleObserver,options);
          if(observerRef.current) observer.observe(observerRef.current);
-    },[handleObserver,getPhotos,page])
+    },[page])
 
 
   return (
     <div className="container" >
          <div style={{minHeight:"800px"}}>
+            <div className="grid-container">
             {photos.map(pic=>(
-                <span key={pic.id+Math.random()} style={{margin:"10px"}} >
-                <img src={pic.url} height="100px" width="200px" alt="img"/>
-                </span>
+                <div key={`${pic.id}+${Math.random()}`} style={{margin:"10px"}} className='grid-item' >
+                <img src={pic.url} height="300px" maxWidth="auto" alt="img"/>
+                </div>
             ))}
-
+            </div>
         </div> 
         <div ref={observerRef} style={loadingCSS}>
             <span style={{display:loading?"block":"none"}}>Loading...</span>
-            {console.log("photos",photos)}
         </div>
-        {console.log(photos)}
+        {console.log(page)}
     </div>
   )
 }
